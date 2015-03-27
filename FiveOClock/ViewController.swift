67f3b908dc326a66a1bd6fe73608c3146ff7d7eb
mollietaylor <mollie.taylor@gmail.com>
2015-03-27 11:00:09 -0400

    Initial Commit

---
+++ FiveOClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FiveOClock
+//
+//  Created by Mollie on 3/27/15.
+//  Copyright (c) 2015 Proximity Viz LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
